aeeba71edde37a9de7eb79de16876c30ed11b2f6
maryok9 <maryonyi144@gmail.com>
2022-10-14 15:46:56 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Print combinations of two digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tens;
+	int ones;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		putchar(tens + '0');
+		putchar(ones + '0');
+		if (tens < 8)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
