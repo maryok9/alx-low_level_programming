aa12f4a9a04bcd4c48b3fde5cf7fbd7467aa64cd
maryok9 <maryonyi144@gmail.com>
2022-10-18 14:37:07 -0700

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,17 +7,17 @@
  */
 int print_last_digit(int n)
 {
-	int 1;
+	int i;
 
-	1 = n % 10;
-	if (1 < 0)
+	i = n % 10;
+	if (i < 0)
 	{
-		_putchar(-1 + 48);
-		return (-1);
+		_putchar(-i + 48);
+		return (-i);
 	}
 	else
 	{
-		_putchar(1 + 48);
-		return (1);
+		_putchar(i + 48);
+		return (i);
 	}
 }
