a7beb9e678c587eca9f838eb334034a60c41e3bb
maryok9 <maryonyi144@gmail.com>
2022-10-24 06:00:00 -0700

    print-arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - update value
+ * @a: input
+ * @n: value
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", a[x])
+			if (x != n - 1)
+			{
+				printf(",");
+			}
+	}
+	printf("\n");
+}
